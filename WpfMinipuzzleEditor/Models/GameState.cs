58ab5ecf379cf7f1facc79aca7ae21e3db014950
phm0423 <qkrgusalsok@gmail.com>
2025-06-18 17:22:50 +0900

    클래스정의

---
+++ WpfMinipuzzleEditor/Models/GameState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMinipuzzleEditor.Models
+{
+    public class GameState
+    {
+        public int PlayerX { get; set; }
+        public int PlayerY { get; set; }
+        public bool isClear { get; set; }
+    }
+}
